f4b0aa0a9f007070ed965b0468c0e35945c5166e
Steven Williams <steven.williams@lantanagroup.com>
2025-03-04 21:19:25 -0500

    Initial commit

---
+++ src/test/java/com/lantanagroup/sandbox_azure/SandboxAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lantanagroup.sandbox_azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
