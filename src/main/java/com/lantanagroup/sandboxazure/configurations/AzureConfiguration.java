07a103fe976420c57f56683fb5ea9f00dc51a61c
Steven Williams <steven.williams@lantanagroup.com>
2025-03-12 22:06:17 -0400

    Support Key Vault references

--- src/main/java/com/lantanagroup/sandboxazure/configurations/AzureConfiguration.java
+++ src/main/java/com/lantanagroup/sandboxazure/configurations/AzureConfiguration.java
@@ -1,16 +1,12 @@
 package com.lantanagroup.sandboxazure.configurations;
 
 import com.azure.identity.DefaultAzureCredentialBuilder;
-import com.azure.spring.cloud.appconfiguration.config.ConfigurationClientCustomizer;
+import com.azure.spring.cloud.appconfiguration.config.SecretClientCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
 public class AzureConfiguration {
     @Bean
-    @Profile("default-credential")
-    public ConfigurationClientCustomizer configurationClientCustomizer() {
+    public SecretClientCustomizer secretClientCustomizer() {
         return ((builder, endpoint) -> builder.credential(new DefaultAzureCredentialBuilder().build()));
     }
 }
