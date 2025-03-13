9f38aa2b6bde9f95c50ec8f52629f140be9ab522
Steven Williams <steven.williams@lantanagroup.com>
2025-03-13 14:27:17 -0400

    Use default credential for App Configuration

--- src/main/java/com/lantanagroup/sandboxazure/configurations/AzureConfiguration.java
+++ src/main/java/com/lantanagroup/sandboxazure/configurations/AzureConfiguration.java
@@ -1,12 +1,18 @@
 package com.lantanagroup.sandboxazure.configurations;
 
 import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.spring.cloud.appconfiguration.config.ConfigurationClientCustomizer;
 import com.azure.spring.cloud.appconfiguration.config.SecretClientCustomizer;
 import org.springframework.context.annotation.Bean;
 
 public class AzureConfiguration {
     @Bean
+    public ConfigurationClientCustomizer configurationClientCustomizer() {
+        return (builder, endpoint) -> builder.credential(new DefaultAzureCredentialBuilder().build());
+    }
+
+    @Bean
     public SecretClientCustomizer secretClientCustomizer() {
-        return ((builder, endpoint) -> builder.credential(new DefaultAzureCredentialBuilder().build()));
+        return (builder, endpoint) -> builder.credential(new DefaultAzureCredentialBuilder().build());
     }
 }
